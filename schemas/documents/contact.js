17f392bb1155a55d4488850ac50ec998aa15a6a0
David Matthews <dwm503@gmail.com>
2020-05-31 14:54:50 +0100

    feature: add twitterUrl to contact document

--- schemas/documents/contact.js
+++ schemas/documents/contact.js
@@ -1,27 +1,32 @@
 export default {
-    name: 'contact',
-    title: '問い合わせ',
-    type: 'document',
-    fields: [
-        {
-            name: 'contactBody',
-            title: '問い合わせ文章',
-            type: 'localeBlock'
-        },
-        {
-            name: 'follow',
-            title: 'フォロー文章',
-            type: 'localeString'
-        },
-        {
-            name: 'instagramUrl',
-            title: 'インストグラムのリンク',
-            type: 'url'
-        },
-        {
-            name: 'facebookUrl',
-            title: 'フェイスブックのリンク',
-            type: 'url'
-        }
-    ],
+  name: 'contact',
+  title: '問い合わせ',
+  type: 'document',
+  fields: [
+    {
+      name: 'contactBody',
+      title: '問い合わせ文章',
+      type: 'localeBlock',
+    },
+    {
+      name: 'follow',
+      title: 'フォロー文章',
+      type: 'localeString',
+    },
+    {
+      name: 'instagramUrl',
+      title: 'インストグラムのリンク',
+      type: 'url',
+    },
+    {
+      name: 'facebookUrl',
+      title: 'フェイスブックのリンク',
+      type: 'url',
+    },
+    {
+      name: 'twitterUrl',
+      title: 'ツイッターのリンク',
+      type: 'url',
+    },
+  ],
 }
